3994904cfbc552dc380c6cf04496a2a0c2c87042
Nguyen Cao Trung <trung.nguyen2@trginternational.com.vn>
2016-07-21 16:03:14 +0700

    Add naviation

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack.config.js');
 
 module.exports = function (config) {
 	config.set({
-		browswers: ['Chrome'],
+		browsers: ['Chrome'],
 		singleRun: true,
 		frameworks: ['mocha'],
 		files: ['app/test/**/*.test.jsx'],
